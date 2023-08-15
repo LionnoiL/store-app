95f54e4cd8d9456f02b27a4205e3ffd7abe500f2
Andriy Gaponov <wmconstructor@gmail.com>
2023-08-15 19:03:21 +0300

    initial commit

---
+++ src/main/resources/db/migration/V2023.08.23.001__database_init.sql
@@ -0,0 +1,46 @@
+create TABLE products (
+    product_guid            VARCHAR (36)  NOT NULL PRIMARY KEY,
+    product_name            VARCHAR (150) NOT NULL,
+    product_code            VARCHAR (11),
+    weight                  BOOLEAN DEFAULT (0),
+    base_unit_name          VARCHAR (150) NOT NULL
+);
+
+
+create TABLE orders (
+    order_guid                  VARCHAR (36) PRIMARY KEY,
+    order_number                BIGINT       NOT NULL,
+    order_date                  TIMESTAMP    DEFAULT (CURRENT_TIMESTAMP),
+    summa_doc                   DOUBLE,
+    agent_name                  VARCHAR (150) NULL,
+    order_comment               VARCHAR (150) NULL
+);
+
+
+create TABLE orders_detail (
+    orders_detail_guid      VARCHAR (36) PRIMARY KEY,
+    order_guid              VARCHAR (36) NOT NULL,
+    line_number             INT,
+    product_guid            VARCHAR (36) NOT NULL,
+    qty                     DOUBLE,
+    qty_collected           DOUBLE,
+    price                   DOUBLE,
+    summa                   DOUBLE       DEFAULT (0),
+    CONSTRAINT orders_detail_fk_orders FOREIGN KEY (order_guid) REFERENCES orders(order_guid),
+    CONSTRAINT orders_detail_fk_productss FOREIGN KEY (product_guid) REFERENCES products(product_guid)
+);
+
+
+CREATE TABLE users (
+	id                      bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	user_name               VARCHAR(255),
+	password                VARCHAR(255),
+	role                    VARCHAR(36) NOT NULL,
+	enabled                 BOOLEAN DEFAULT false
+);
+
+create unique index ix_users_user_name   on users (user_name);
+
+
+INSERT INTO users (user_name, password, role)
+VALUES ('admin', '$2a$10$5qBqaBeWBCgn61.C1A7MtejTofjljRTMsLs5NhG55cQ2sSeGIZA0u', 'ROLE_ADMIN');
