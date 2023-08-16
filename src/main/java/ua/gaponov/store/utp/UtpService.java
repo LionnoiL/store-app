d85a1b100a911ca4edb02e91c52b9761c86875ff
Andriy Gaponov <wmconstructor@gmail.com>
2023-08-16 15:28:12 +0300

    fix utp service. add user param.

--- src/main/java/ua/gaponov/store/utp/UtpService.java
+++ src/main/java/ua/gaponov/store/utp/UtpService.java
@@ -1,8 +1,11 @@
 package ua.gaponov.store.utp;
 
+import lombok.RequiredArgsConstructor;
 import org.jsoup.Jsoup;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import ua.gaponov.store.user.User;
+import ua.gaponov.store.user.UserService;
 import ua.gaponov.store.utils.JsonConverter;
 
 import java.io.IOException;
@@ -12,9 +15,11 @@ import java.util.List;
 /**
  * @author Andriy Gaponov
  */
+@RequiredArgsConstructor
 @Service
 public class UtpService {
 
+    private final UserService userService;
     @Value("${utp.url}")
     private String utpHttpServerUrl;
     @Value("${utp.user.name}")
@@ -24,11 +29,14 @@ public class UtpService {
 
     public List<OrderRequestDto> getOrdersList() {
 
+        User currentUser = userService.getCurrentUser();
+
         try {
+            String url = utpHttpServerUrl + "?userName=" + currentUser.getUserName();
             String login = userName + ":" + userPassword;
             String base64login = Base64.getEncoder().encodeToString(login.getBytes());
 
-            String response = Jsoup.connect(utpHttpServerUrl)
+            String response = Jsoup.connect(url)
                     .header("Authorization", "Basic " + base64login)
                     .ignoreContentType(true).get().body().text();
             List<OrderRequestDto> ordersList = JsonConverter.convertJsonStringToList(
